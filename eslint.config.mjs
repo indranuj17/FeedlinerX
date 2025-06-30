af8d15a07456ec1d31f85f0fc162b2d5d10a7613
Indranuj <indranujdev015@gmail.com>
2025-07-01 01:14:01 +0530

    commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,8 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
-import js from "@eslint/js";
-import tseslint from "typescript-eslint";
+
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,25 +11,10 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-// Combine legacy configs and your own custom override
+
+
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-
-  {
-    files: ["**/*.ts", "**/*.tsx", "**/*.js", "**/*.jsx"],
-    languageOptions: {
-      parserOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module",
-      },
-    },
-    plugins: {
-      "@typescript-eslint": tseslint.plugin,
-    },
-    rules: {
-      "@typescript-eslint/no-unused-vars": "off", // ✅ disables warning
-    },
-  },
 ];
 
 export default eslintConfig;
